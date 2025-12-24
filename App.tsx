ba54a52d2408807b69fa4193d236d912deed8c33
Gaurav Dhamija onlinepartnership.nl <gaurav@onlinepartnership.nl>
2025-12-24 19:14:56 +0530

    Async Storage Library added for local storage data update with pod installs new also.

--- App.tsx
+++ App.tsx
@@ -4,8 +4,29 @@ import { View, Text } from 'react-native';
 import { NavigationContainer,Link } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Button } from '@react-navigation/elements';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+
 
 function HomeScreen() {
+
+          const storeData = async (value: string) => {
+            try {
+              await AsyncStorage.setItem('my-key', value);
+            } catch (e) {
+              // saving error
+            }
+          };
+
+          const storeData2 = async (value) => {
+            try {
+              const jsonValue = JSON.stringify(value);
+              await AsyncStorage.setItem('my-key', jsonValue);
+            } catch (e) {
+              // saving error
+            }
+          };
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
@@ -43,5 +64,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
-
